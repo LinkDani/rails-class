1d63c53379484057e849011f811a6f30a2aadb47
danielwalterpand <danielwalterpand@gmail.com>
2018-03-08 19:40:40 -0500

    About

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   def index
     @nome = 'Lightsaber'
     @product = Product.last
+    
+    @numero = params['number'] || 0
+    quantidade = params['quantity'] || 0
+    @produto_encontrado = Product.find_by_id(@numero)
+    @valor_total =  quantidade.to_f * @produto_encontrado.price
   end
   
   def about
